deda52dbcd39599c682480f7bb0198d35d75ad3a
little_penguin66 <littlepenguinzq@gmail.com>
2024-05-02 17:31:04 +0800

    Site updated: 2024-05-02 17:31:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Markdown/页面配置/","Markdown/anzhiyu添加新页面基于已有模板/"];function toRandomPost(){
+var posts=["Markdown/页面配置/","Markdown/anzhiyu添加新页面基于已有模板/","芝士学习/激活函数/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
