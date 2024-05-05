b8bafee995472c30cad48885768bd7b915ec7ff6
little_penguin66 <littlepenguinzq@gmail.com>
2024-05-05 13:53:18 +0800

    Site updated: 2024-05-05 13:53:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Markdown/页面配置/","Markdown/anzhiyu添加新页面基于已有模板/","芝士学习/激活函数/"];function toRandomPost(){
+var posts=["Markdown/页面配置/","Markdown/anzhiyu添加新页面基于已有模板/","杂学/激活函数/","杂学/python打包成.exe/","uncategorized/数学物理方程第一章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
