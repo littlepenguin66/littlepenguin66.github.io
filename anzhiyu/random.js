13a341d6ab74ccd84bb0e6964bdd4ba43c2c8790
little_penguin66 <littlepenguinzq@gmail.com>
2024-05-06 00:13:04 +0800

    Site updated: 2024-05-06 00:13:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Markdown/页面配置/","Markdown/anzhiyu添加新页面基于已有模板/","杂学/激活函数/","杂学/python打包成.exe/","uncategorized/数学物理方程第一章/","uncategorized/AAAMLprob/index/","uncategorized/AAAMLprob/交叉检验/","uncategorized/AAAMLprob/特征工程/","uncategorized/AAAMLprob/组织机器学习项目/","uncategorized/AAAMLprob/处理分类变量/","uncategorized/AAAMLprob/超参数优化/","uncategorized/AAAMLprob/特征选择/","uncategorized/AAAMLprob/javascripts/katex/","uncategorized/AAAMLprob/评估指标/"];function toRandomPost(){
+var posts=["Markdown/anzhiyu添加新页面基于已有模板/","杂学/python打包成.exe/","uncategorized/数学物理方程第一章/","杂学/激活函数/","Markdown/页面配置/","杂学/AAAMLprob/index/","杂学/AAAMLprob/处理分类变量/","杂学/AAAMLprob/特征工程/","杂学/AAAMLprob/特征选择/","杂学/AAAMLprob/组织机器学习项目/","杂学/AAAMLprob/评估指标/","杂学/AAAMLprob/超参数优化/","杂学/AAAMLprob/交叉检验/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
