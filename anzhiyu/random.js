9bdc529dc0bbdc2cfda74a07cf1c5004bb11ec65
little_penguin66 <littlepenguinzq@gmail.com>
2024-05-06 00:32:19 +0800

    Site updated: 2024-05-06 00:32:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Markdown/anzhiyu添加新页面基于已有模板/","杂学/python打包成.exe/","uncategorized/数学物理方程第一章/","杂学/激活函数/","Markdown/页面配置/","杂学/AAAMLprob/index/","杂学/AAAMLprob/处理分类变量/","杂学/AAAMLprob/特征工程/","杂学/AAAMLprob/特征选择/","杂学/AAAMLprob/组织机器学习项目/","杂学/AAAMLprob/评估指标/","杂学/AAAMLprob/超参数优化/","杂学/AAAMLprob/交叉检验/"];function toRandomPost(){
+var posts=["Markdown/anzhiyu添加新页面基于已有模板/","杂学/python打包成.exe/","uncategorized/数学物理方程第一章/","杂学/激活函数/","Markdown/页面配置/","杂学/AAAMLprob/index/","杂学/AAAMLprob/处理分类变量/","杂学/AAAMLprob/特征工程/","杂学/AAAMLprob/特征选择/","杂学/AAAMLprob/组织机器学习项目/","杂学/AAAMLprob/评估指标/","杂学/AAAMLprob/超参数优化/","杂学/AAAMLprob/交叉检验/","杂学/AAAMLprob/准备环境/","杂学/AAAMLprob/可重复代码和模型方法/","杂学/AAAMLprob/图像分类和分割方法/","杂学/AAAMLprob/无监督和有监督学习/","杂学/AAAMLprob/文本分类或回归方法/","杂学/AAAMLprob/组合和堆叠方法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
